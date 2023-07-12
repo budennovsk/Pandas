df647d4006606df95265c37202295cae29f8288e
Zin <svetloewill@mail.ru>
2023-07-12 11:37:46 +0300

    4

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+
+
 import pandas as pd
 import numpy as np
+from io import StringIO
 
 # df = pd.DataFrame([[1, 'Bob', 'Builder'],
 #                    [2, 'Sally', 'Baker'],
@@ -48,7 +51,7 @@ import numpy as np
 # print('____________')
 #
 
-df = pd.read_csv('E:\\яндекс загрузка\\file-csv.csv', sep=',', encoding='utf-8')
+df = pd.read_csv('E:/яндекс загрузка/file-csv.csv', sep=',', encoding='utf-8', usecols=['quarter', 'SER_REF'])
 
 print(df.dtypes)
 # print(df.loc[0:5].iloc[0:10])
@@ -66,5 +69,20 @@ new_row = pd.Series(
 # df._append(new_row,ignore_index=True)
 # df.drop([0],inplace=True)
 # df.drop(['industry_code','filled jobs','filled jobs revised','filled jobs diff','filled jobs % diff','total_earnings','total earnings revised','earnings diff','earnings % diff'], axis=1, inplace=True)
-df.iloc[:10].drop([6],inplace = True)
+# df.iloc[:10].drop([6],inplace = True)
 print(df)
+
+csv_d1 = 'col_A, col_B, col_C\na1, b1, c1\na2, b2, c2'
+
+df = pd.read_csv(StringIO(csv_d1))
+print(df)
+print("_____________")
+json_buf='[[12, 24, 14, 17], [1, 54, 25, 83], [65,35, 12, 72]]'
+fox = pd.read_json(json_buf, orient='value')
+print(fox.agg(['sum', 'mean', 'std'], axis=1))
+print(pd.get_option('display.encoding'))
+def gou():
+    return 25+25
+gou.__name__='loo'
+print(gou.__name__)
+
---
+++ testfile.py
@@ -0,0 +1,29 @@
+import os
+import pyodbc
+from config import *
+# pth = 'E:/Тестовые задания/'
+# arr = os.listdir(pth)
+# print(arr,'ddd')
+# for i in arr:
+#     print(i)
+#
+# car = os.walk(pth)
+# for r, d,f in car:
+#     print(f)
+
+conn = pyodbc.connect(f"""
+Driver={{{DRIVER_NAME}}};
+Server={SERVER_NAME};
+Database={DATA_BASE};
+Trusted_Connection=yes;
+""")
+
+cur = conn.cursor()
+print(cur)
+cur.execute('SELECT id FROM Table_1')
+v = cur.fetchone()
+print(v.id)
+b = cur.execute("""CREATE TABLE asfdf
+(name VARCHAR(50),  age INTEGER)""")
+b.commit()
+conn.close()
