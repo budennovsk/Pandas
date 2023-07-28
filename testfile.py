87ee9a28c6f89036d2386760c72b2d35cf9a6eef
Zin <svetloewill@mail.ru>
2023-07-28 16:58:20 +0300

    новы1

--- testfile.py
+++ testfile.py
@@ -1,29 +1,31 @@
-import os
-import pyodbc
-from config import *
-# pth = 'E:/Тестовые задания/'
-# arr = os.listdir(pth)
-# print(arr,'ddd')
-# for i in arr:
-#     print(i)
-#
-# car = os.walk(pth)
-# for r, d,f in car:
-#     print(f)
 
-conn = pyodbc.connect(f"""
-Driver={DRIVER_NAME};
-Server={SERVER_NAME};
-Database={DATA_BASE};
-Trusted_Connection=yes;
-""")
+def gor():
+    fff= 1
+    return fff
+def fe(name):
 
-cur = conn.cursor()
-print(cur)
-cur.execute('SELECT id FROM Table_1')
-v = cur.fetchone()
-print(v.id)
-b = cur.execute("""CREATE TABLE asfdf
-(name VARCHAR(50),  age INTEGER)""")
-b.commit()
-conn.close()
+    ds = 1
+
+    one = []
+    two = []
+    free = []
+    if name == 'one':
+        one = gor()+(1+ds)
+
+
+    if name == 'two':
+        two = gor() + (2+ds)
+
+
+    if name == 'free':
+        free = gor() + (3+ds)
+    return one, two, free
+for i in ['one', 'two', 'free']:
+
+    # on,tw,tre=fe(i)
+    # print(on,tw,tre)
+    on, tr, g = fe(i)
+    print(on)
+    # next(on)
+    # print([i for i in on])
+
