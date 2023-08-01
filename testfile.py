c6a445df0d3eb16224171f6a1835f73fdde46e77
Zin <svetloewill@mail.ru>
2023-08-01 18:50:35 +0300

    новы3

--- testfile.py
+++ testfile.py
@@ -1,31 +1,42 @@
+import pandas as pd
+
 
 def gor():
-    fff= 1
+    fff = 1
     return fff
-def fe(name):
 
+
+def fe(name):
     ds = 1
 
     one = []
     two = []
     free = []
     if name == 'one':
-        one = gor()+(1+ds)
-
+        one = gor() + (1 + ds)
 
     if name == 'two':
-        two = gor() + (2+ds)
-
+        two = gor() + (2 + ds)
 
     if name == 'free':
-        free = gor() + (3+ds)
+        free = gor() + (3 + ds)
     return one, two, free
-for i in ['one', 'two', 'free']:
 
+
+for i in ['one', 'two', 'free']:
     # on,tw,tre=fe(i)
     # print(on,tw,tre)
     on, tr, g = fe(i)
     print(on)
     # next(on)
     # print([i for i in on])
-
+data = [{'Articul3': 'aaaa',
+         'Group_name': 'bbbb',
+         'Format_TT': 'ccccc'
+         }]
+df = pd.DataFrame(data=data)
+print(df)
+print(df.columns)
+print(df.columns.values)
+if 'Format_TT' in df.columns.values:
+    print('ddd')
