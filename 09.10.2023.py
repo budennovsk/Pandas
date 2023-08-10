b78f9015458a99f2608e8709d771e24a5ac119b5
Python <svetloewill@mail.ru>
2023-08-10 19:33:51 +0300

    ура)ю

--- 09.10.2023.py
+++ 09.10.2023.py
@@ -1,2 +1,22 @@
-for row in range(10):
-    print(row)
+
+import pandas as pd
+
+DATA = []
+dict_data = {}
+dfd = pd.read_excel('sales_Manufacture__all_3_HYPER_Печенье_Дискаунтер.xlsx')
+print(dfd)
+mask = dfd[(dfd['Measures'] == 'MARKET_Sales_kg')]
+for index, row in mask.iterrows():
+    # print(row.index[1:])
+    dict_data = {}
+    dict_data['dd']= list(row.values)
+    print(dict_data)
+
+
+# mask1 = dfd[(dfd['Measures'] == 'MARKET_DIFFERENCE_Sales_kg')]
+# mask2 = dfd[(dfd['Measures'] == 'MARKET_DIFFERENCE_PERCENT_Sales_kg')]
+#
+#
+# DATA.append([mask, mask1, mask2])
+
+# print(DATA)
