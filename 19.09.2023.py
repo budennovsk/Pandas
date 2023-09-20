ed8a87587ccdfafa46266d56f6ed06f9f90c4e50
Python <svetloewill@mail.ru>
2023-09-20 13:03:58 +0300

    header

--- 19.09.2023.py
+++ 19.09.2023.py
@@ -1,16 +1,64 @@
 import pandas as pd
-res = pd.read_excel('ML_manufacture_first_all_10_HYPER_Батончики_Супермаркет.xlsx')
-print(res)
+import xlsxwriter
 
-X_col = res.columns[6:]
-print(X_col)
-y_col = 'Percentage_Sales_rub'
-print(res[y_col])
-min_index = res[y_col].idxmin()
-# Выводим всю строку с минимальным значением
-min_row = res.iloc[min_index, 6:].values.tolist()
-print(min_row)
+file_name = 'optimize.xlsx'
 
 
-min_row.insert(2, 'min_row')
-print(pd.DataFrame(min_row))
+
+data1 = pd.DataFrame({'col1':[1,2,3],
+                      'col': [4,5,6]})
+data_col_name = data1.columns
+data2 = pd.DataFrame({'col1':[111,21,31],
+                      'col': [34,53,62]})
+data_col_name_1 = data2.columns
+
+data3 = pd.DataFrame({'col1':[1555,5552,55553],
+                      'col': [4555,5,6]})
+
+workbook = xlsxwriter.Workbook(filename=file_name)
+worksheet = workbook.add_worksheet()
+
+
+
+
+start_row = 0  # Начальная строка для записи данных
+
+
+col = 0
+
+
+# Запишите названия колонок
+worksheet.write_row(start_row , col, data_col_name)
+start_row+=1
+
+# Запись значений первого DataFrame
+for row in range(len(data1)):
+    for col in range(len(data1.columns)):
+        worksheet.write(start_row + row, col, data1.iloc[row, col])
+
+# Перемещение к следующей строке для записи значений следующего DataFrame
+start_row += len(data1) + 2
+
+worksheet.write_row(start_row , col-1, data_col_name_1)
+start_row+=1
+
+# Запись значений второго DataFrame
+for row in range(len(data2)):
+    for col in range(len(data2.columns)):
+        worksheet.write(start_row + row, col, data2.iloc[row, col])
+
+# Перемещение к следующей строке для записи значений третьего DataFrame
+start_row += len(data2) + 3
+
+worksheet.write_row(start_row , col-1, data_col_name_1)
+start_row+=1
+
+# Запись значений третьего DataFrame
+for row in range(len(data3)):
+    for col in range(len(data3.columns)):
+        worksheet.write(start_row + row, col, data3.iloc[row, col])
+
+
+
+# Закрытие файла Excel
+workbook.close()
