9ed05cb3cec31d8e487c12a1da3cea2691f6a796
Python <svetloewill@mail.ru>
2023-11-01 18:41:35 +0300

    Tenserfloy

--- tenser_4.py
+++ tenser_4.py
@@ -1,119 +1,57 @@
-from sklearn.preprocessing import StandardScaler
+
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# import numpy as np
+# from keras.layers import LeakyReLU
+# import tensorflow as tf
+#
+# # Создание слоя ELU
+# elu_layer = LeakyReLU(alpha=0.01)
+#
+# # Пример входных данных
+#
+#
+# # Пример входных данных
+# input_data = np.array([1, 2, 3,-200, -4])
+# reshaped_tensor = tf.expand_dims(input_data, axis=0)
+#
+# # Применение слоя ELU к входным данным
+# output_data = elu_layer(reshaped_tensor)
+#
+#
+# print("Входные данные:", input_data)
+# print("Выходные данные:", output_data)
+#
+# import numpy as np
+# from keras.layers import Input,  LayerNormalization
+# from keras.models import Model
+#
+# # Создаем входной тензор
+# batch_size = 3
+# features = 4
+# input_tensor = np.array([[1, 2, 3, 4], [5, 6, 7, 22], [9, 10, 11, 12]])
+#
+# # Создаем слой BatchNormalization
+# bn_layer = LayerNormalization(axis=-1)
+#
+# # Применяем нормализацию к входному тензору
+# normalized_tensor = bn_layer(input_tensor)
+# print(normalized_tensor)
+
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling
 import numpy as np
 
-# Пример данных
-data = np.array([[1, 22], [3, 4], [5, 6], [7, 8]])
-data1 = np.array([[1, 2], [4, 5]])
+# Create an instance of the Rescaling layer
+rescaling_layer = Rescaling(scale=1./255)  # Example scale value for image data
 
+# Data to be rescaled
+data = np.array([[100, 150, 200], [50, 75, 100]])
 
-# Инициализация объекта стандартизации
-scaler = StandardScaler()
+# Rescale the data
+scaled_data = rescaling_layer(data)
 
-# Применение стандартизации к данным
-# scaled_data = scaler.fit_transform(data)
-scaled_data1 = scaler.fit_transform(data)
-
-
-
- # Вывод результатов
-print("Исходные данные:")
-print(data)
-
-print("\nСтандартизованные данные:")
-
-
-print(scaled_data1)
-
-se1 = scaler.inverse_transform(scaled_data1)
-print(se1)
-
-
-
-print('___')
-
-import numpy as np
-
-def normalize(data):
-    min_val = np.min(data)
-    max_val = np.max(data)
-    normalized_data = (data - min_val) / (max_val - min_val)
-    return normalized_data
-print(normalize(data))
-'''[[0.00000000e+00 1.00000000e+00]
- [9.00495272e-04 1.35074291e-03]
- [1.80099054e-03 2.25123818e-03]
- [2.70148582e-03 3.15173345e-03]]'''
-
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-
-# Создание DataFrame с числовыми данными
-data3 = {'A': [10, 20, 30, 40],
-        'B': [1, 2, 3, 4],
-        'C': [100, 200, 300, 400]}
-df = pd.DataFrame(data3)
-
-# Создание экземпляра класса StandardScaler
-scaler = StandardScaler()
-
-# Подгонка и трансформация данных
-normalized_data = scaler.fit_transform(df)
-
-# Конвертация в новый DataFrame
-normalized_df = pd.DataFrame(normalized_data, columns=df.columns)
-
-# Вывод нормализованного DataFrame
-print(normalized_df)
-print('_____________')
-
-n = 0
-x = np.expand_dims([[1, 2] ,[3, 4]], axis=0)
-print(x)
-print(x.shape)
-
-ff = 12
-dd = 12
-cc =1
-
-mask = ff==cc
-print(mask)
-
-from sklearn.metrics import mean_squared_log_error, mean_squared_error
-from sklearn.metrics import mean_absolute_error
-# y_true - фактические значения, y_pred - предсказанные значения
-y_true = [3, 5, 2.5, 9]
-y_pred = [2.5, 5, 4, 8]
-
-msle = mean_squared_log_error(y_true, y_pred)
-print(f'MSLE: {msle}')
-mse = mean_squared_error(y_true, y_pred)
-print(f'MSE: {mse}')
-mae = mean_absolute_error(y_true, y_pred)
-print(f'MAE: {mae}')
-
-
-
-
-import matplotlib.pyplot as plt
-
-# Your code here
-
-
-
-print('______')
-
-start = [1, 3, 5, 7]
-stop = [2, 4, 6, 8]
-interval_colors = ['red', 'green', 'blue', 'yellow']
-
-
-for s, e, color in zip(start, stop, interval_colors):
-
-    plt.axvspan(s, e, facecolor=color, alpha=0.3, label='Праздничные дни')
-plt.legend()
-
-plt.show()
-
-
+print(scaled_data)
 
 
