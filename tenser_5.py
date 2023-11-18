ab57a18cdd13b2419ee081a5bd6c82460841eb5f
Python <svetloewill@mail.ru>
2023-11-18 22:49:57 +0300

    Tenserfloy

--- tenser_5.py
+++ tenser_5.py
@@ -57,52 +57,144 @@ from keras.preprocessing.sequence import TimeseriesGenerator
 # sales_2 = [[4,4,4],[5,5,5],[6,6,6]]
 #
 # sales_3 = [[7,7,7],[8,8,8],[9,9,9]]
+# import numpy as np
+# from keras.models import Sequential
+# from keras.layers import LSTM, Dense
+#
+# # Ваши исходные данные
+# sales_1 = [[1,1,1],[2,2,2],[3,3,3]]
+# sales_2 = [[4,4,4],[5,5,5],[6,6,6]]
+# sales_3 = [[7,7,7],[8,8,8],[9,9,9]]
+#
+# # Объедините все данные в один список
+# data = [sales_1, sales_2, sales_3]
+#
+# # Преобразуйте список в numpy массив
+# data = np.array(data)
+# print('all',data.shape)
+# # Подготовьте данные
+# prepared_data = []
+# for i in range(data.shape[0]):
+#     current_set = []
+#     for j in range(data.shape[1]-1):
+#         current_set.append(data[i, j:j+2, :])
+#     prepared_data.append(current_set)
+# prepared_data = np.array(prepared_data)
+#
+# # Разделите данные на входные данные (X) и целевую переменную (y)
+# X = prepared_data[:, :, 0, :]  # все данные на шаге t
+#
+# y = prepared_data[:, :, 1, 0]  # только 'Regular_Sales_Promo_Sales' на шаге t+1
+#
+# # Определите модель
+# model = Sequential()
+# model.add(LSTM(50, activation='relu', input_shape=(2, 3)))
+# model.add(Dense(1))
+#
+# # Скомпилируйте модель
+# model.compile(optimizer='adam', loss='mse')
+# print(X.shape)
+# print(X)
+# print(y.shape)
+# print(y)
+# # Обучите модель
+# model.fit(X, y, epochs=2, verbose=0)
+#
+#
+# # Предскажите значения с помощью модели
+# sales_pred = model.predict(X)
+#
+# # Выведите предсказанные значения
+# print(sales_pred)
+#
+# def split_data(df, test_split=0.15):
+#     n = int(len(df) * test_split)
+#     train, test = df[:-n], df[-n:]
+#     return train, test
+
+# import numpy as np
+#
+# route_distances = np.array([[0, 1, 2],
+#                             [3, 4, 5],
+#                             [6, 7, 8]])
+# import numpy as np
+#
+# route_distances = np.array([[0, 1, 2],
+#                             [3, 4, 5],
+#                             [6, 7, 8]])
+# print(route_distances[:,1:
+#
+#       ])
+#
+# import numpy as np
+# from keras.models import Sequential
+# from keras.layers import LSTM, Dense
+#
+# # Данные
+# sku_1 = [1,2,3,4,5,6,7,8,9,10]
+# sku_2 = [10,20,30,40,50,60,70,80,90,100]
+# sku_3 = [100,200,300,400,500,600,700,800,900,1000]
+# sku_4 = [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]
+# sku_5 = [10000,20000,30000,40000,50000,60000,70000,80000,90000,100000]
+#
+# # Преобразование в numpy массивы
+# sku_1 = np.array(sku_1)
+# print(sku_1)
+#
+#
+# sku_2 = np.array(sku_2)
+# sku_3 = np.array(sku_3)
+# sku_4 = np.array(sku_4)
+# sku_5 = np.array(sku_5)
+#
+# # Объединение SKU в один массив
+# data = np.stack((sku_1, sku_2, sku_3, sku_4, sku_5), axis=-1)
+# print(data.shape)
+# print(data)
+#
+#
+#
+#
+# # Формирование входных данных для LSTM
+# X = data[:-1, :]  # Все данные, кроме последнего шага
+# y = data[1:, :]  # Следующий шаг
+# print(X)
+# print(y)
+#
+#
+# # Преобразование в форму [samples, time steps, features]
+# X = np.reshape(X, (X.shape[0], 1, X.shape[1]))
+# y = np.reshape(y, (y.shape[0], y.shape[1]))
+# print(X.shape)
+# print(y.shape)
+# # Создание LSTM модели
+# model = Sequential()
+# model.add(LSTM(50, activation='relu', input_shape=(1, 5)))
+# model.add(Dense(5))
+# model.compile(optimizer='adam', loss='mse')
+#
+# # Обучение модели
+# model.fit(X, y, epochs=200, verbose=0)
+# # Предсказание для следующего шага
+# last_step = X[-1,:,:]  # последний доступный шаг в данных
+# last_step = np.reshape(last_step, (1, 1, 5))  # преобразование в форму, ожидаемую моделью
+# print(last_step.shape)
+#
+#
+# next_step = model.predict(last_step)  # предсказание следующего шага
+#
+# print(next_step)
+
 import numpy as np
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
 
-# Ваши исходные данные
-sales_1 = [[1,1,1],[2,2,2],[3,3,3]]
-sales_2 = [[4,4,4],[5,5,5],[6,6,6]]
-sales_3 = [[7,7,7],[8,8,8],[9,9,9]]
+# Предположим, что array - это ваш исходный массив размером (265, 10)
+array = np.random.rand(265, 10)
 
-# Объедините все данные в один список
-data = [sales_1, sales_2, sales_3]
+# Создаем массив нулей размером (100, 10)
+zeros = np.zeros((100, 10))
 
-# Преобразуйте список в numpy массив
-data = np.array(data)
-print('all',data.shape)
-# Подготовьте данные
-prepared_data = []
-for i in range(data.shape[0]):
-    current_set = []
-    for j in range(data.shape[1]-1):
-        current_set.append(data[i, j:j+2, :])
-    prepared_data.append(current_set)
-prepared_data = np.array(prepared_data)
+# Добавляем массив нулей к исходному массиву
+result = np.vstack((zeros, array))
 
-# Разделите данные на входные данные (X) и целевую переменную (y)
-X = prepared_data[:, :, 0, :]  # все данные на шаге t
-
-y = prepared_data[:, :, 1, 0]  # только 'Regular_Sales_Promo_Sales' на шаге t+1
-
-# Определите модель
-model = Sequential()
-model.add(LSTM(50, activation='relu', input_shape=(2, 3)))
-model.add(Dense(1))
-
-# Скомпилируйте модель
-model.compile(optimizer='adam', loss='mse')
-print(X.shape)
-print(X)
-print(y.shape)
-print(y)
-# Обучите модель
-model.fit(X, y, epochs=2, verbose=0)
-
-
-# Предскажите значения с помощью модели
-sales_pred = model.predict(X)
-
-# Выведите предсказанные значения
-print(sales_pred)
+print(result.shape)  # Выводит: (365, 10)
+print(result)
