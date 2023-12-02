77f515b7b43689f3b030ffd603d0d67b2c078353
Python <svetloewill@mail.ru>
2023-12-02 15:23:14 +0300

    2

--- catiboost.py
+++ catiboost.py
@@ -33,21 +33,104 @@
 #
 # c =[[0]+[100,200,300,365]*2]
 # print(c)
-
+#
+# import pandas as pd
+#
+# # Предположим, что 'df' - это ваш DataFrame, а 'day_of_year' - столбец с номерами дней в году
+# df = pd.DataFrame({'day_of_year': range(1, 731)})  # Пример DataFrame
+#
+# # Ваш список интервалов
+# data = [(10, 24), (47, 61), (62, 76), (280, 294), (302, 316), (378, 392), (418, 432), (426, 440), (644, 658), (668, 682)]
+#
+# # Создаем новый столбец 'data_promo' и заполняем его нулями
+# df['data_promo'] = 0
+#
+# # Проходим по списку интервалов и устанавливаем '1' там, где 'day_of_year' попадает в интервал
+# for start, end in data:
+#     df.loc[(df['day_of_year'] >= start) & (df['day_of_year'] <= end), 'data_promo'] = 1
+#
+# print(df[9:25])
+# df = df.reset_index().merge(df_1year, how='left', on='day_of_year').set_index('index')
 import pandas as pd
 
-# Предположим, что 'df' - это ваш DataFrame, а 'day_of_year' - столбец с номерами дней в году
-df = pd.DataFrame({'day_of_year': range(1, 731)})  # Пример DataFrame
+# df = pd.DataFrame({'promo_merge': [0,0,0,4,5,6,0,0,0,0,0,2,3,4,5,0,0,0,0,0,0,0,0,0,0,0,7,8,9,10]})
+#
+#
+#
+# result = pd.DataFrame({'result':[0,2,3,4,5,6,7,8,9,10]})
 
-# Ваш список интервалов
-data = [(10, 24), (47, 61), (62, 76), (280, 294), (302, 316), (378, 392), (418, 432), (426, 440), (644, 658), (668, 682)]
+# import pandas as pd
+# import numpy as np
+#
+# # Ваш исходный DataFrame
+# df = pd.DataFrame({'promo_merge': [0,0,0,4,5,6,0,0,0,0,0,2,3,4,5,0,0,0,0,0,0,0,0,0,0,0,7,8,9,10]})
+# print(df.shape)
+#
+# # Разделите DataFrame на 3 части и сбросьте индексы
+# dfs = [group.reset_index(drop=True) for _, group in df.groupby(np.arange(len(df)) // 10)]
+#
+# # Получите ненулевые значения из каждого блока
+# non_zero_values = [block[block['promo_merge'] != 0] for block in dfs]
+#
+# # Объедините ненулевые значения
+# result = pd.concat(non_zero_values)
+#
+# print(result)
 
-# Создаем новый столбец 'data_promo' и заполняем его нулями
-df['data_promo'] = 0
+# import pandas as pd
+# import numpy as np
+#
+# # Исходный DataFrame
+# df = pd.DataFrame({'promo_merge': [0,0,0,4,5,6,0,0,0,0,1,0,3,4,5,0,0,0,0,0,0,0,0,4,0,0,7,8,0,10]})
+# df['count'] =[19,19,19,3,2,1,19,19,19,19,1,19,1,3,2,19,19,19,19,19,19,19,19,3,19,19,1,1,19,1]
+# print(df)
+# # Разделите DataFrame на 3 части и сбросьте индексы
+# dfs = [group.reset_index(drop=True) for _, group in df.groupby(np.arange(len(df)) // 10)]
+#
+# # Индексы нулей в первой части
+# zero_indices = dfs[0][dfs[0]['promo_merge'] == 0].index
+# zero_indices_1 = dfs[0][dfs[0]['count'] == 0].index
+#
+# # Замените нули в первой части значениями из второй и третьей частей
+# for i in range(1, len(dfs)):
+#     replace_indices = dfs[i][dfs[i]['promo_merge'] != 0].index
+#     common_indices = np.intersect1d(zero_indices, replace_indices)
+#     dfs[0].loc[common_indices, 'promo_merge'] = dfs[i].loc[common_indices, 'promo_merge'].values
+#     zero_indices = dfs[0][dfs[0]['promo_merge'] == 0].index
+#
+# print(dfs[0])
+# for i in range(1, len(dfs)):
+#     replace_indices = dfs[i][dfs[i]['count'] != 0].index
+#     common_indices = np.intersect1d(zero_indices_1, replace_indices)
+#     dfs[0].loc[common_indices, 'count'] = dfs[i].loc[common_indices, 'count'].values
+#     zero_indices_1 = dfs[0][dfs[0]['count'] == 0].index
+# print('_______________________')
 
-# Проходим по списку интервалов и устанавливаем '1' там, где 'day_of_year' попадает в интервал
-for start, end in data:
-    df.loc[(df['day_of_year'] >= start) & (df['day_of_year'] <= end), 'data_promo'] = 1
+import pandas as pd
+import numpy as np
 
-print(df[9:25])
-df = df.reset_index().merge(df_1year, how='left', on='day_of_year').set_index('index')
+# Исходный DataFrame
+df = pd.DataFrame({'count': [19,19,19,3,2,1,19,19,19,19,1,19,1,3,2,19,19,19,19,19,19,19,19,3,19,19,1,1,19,1]})
+
+# Разделите DataFrame на 3 части
+dfs = np.array_split(df, 3)
+
+# Создаем список для хранения результатов
+result = []
+
+# Цикл по каждому индексу
+for i in range(10):  # мы знаем, что каждый блок имеет 10 строк
+    # Получаем текущее значение из каждого блока
+    values = [dfs[j].iloc[i]['count'] for j in range(3)]
+    # Если значения не одинаковы, выбираем минимальное значение
+    if len(set(values)) > 1:
+        result.append(min(values))
+    else:
+        # Если значения одинаковы, сохраняем значение
+        result.append(values[0])
+
+# Уменьшаем размер DataFrame до 10 строк и добавляем результат
+df = df.iloc[:10]
+df['res'] = result
+
+print(df)
