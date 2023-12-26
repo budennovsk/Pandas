ea530605988d5e454f8e182c96143002d8cb447b
Python <svetloewill@mail.ru>
2023-12-26 22:09:15 +0300

    spamrio

---
+++ rio_sport.py
@@ -0,0 +1,53 @@
+
+from selenium import webdriver
+from fake_useragent import UserAgent
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.webdriver.common.by import By
+from tqdm import tqdm
+
+
+for _ in tqdm(range(100)):
+    # Создание экземпляра UserAgent
+    ua = UserAgent()
+    user_agent = ua.random
+
+    # Опции браузера
+    options = Options()
+    # options.add_argument("--headless")  # Включение headless режима
+
+    options.add_argument(f'user-agent={user_agent}')
+    # options.add_argument("--headless")
+
+    # Путь к драйверу браузера (в данном случае Chrome)
+    driver_path = 'chromeexe/chromedriver.exe'
+
+
+    # Создание экземпляра драйвера
+    driver = webdriver.Chrome()
+
+
+    # Открытие ссылки
+    url = 'https://rsport.ria.ru/20231219/sportswoman-of-year-1909228789.html'
+
+    driver.get(url)
+    time.sleep(2)
+    # element = driver.find_element(By.CLASS_NAME,"text  text_ texo1703613919043 c1703613920162 ts_c1703613920046 c1703613920161")
+    element = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[4]/div[2]/div[7]/div[1]/div[1]')
+    time.sleep(2)
+    element.click()
+    time.sleep(2)
+    element = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[5]/div/div/div[2]/div/div[2]/div/div[8]/div/div/div[2]/div[1]/div[2]/div')
+    time.sleep(2)
+    element.click()
+    time.sleep(2)
+    element = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[6]/div/div/div[1]/div')
+    time.sleep(2)
+    element.click()
+    time.sleep(2)
+
+    # Закрытие драйвера
+    driver.quit()
+
+
+
