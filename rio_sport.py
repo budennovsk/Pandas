70b3f488d8e0ccf4e905daf4f4aa529647911bbb
Python <svetloewill@mail.ru>
2023-12-29 19:03:19 +0300

    spamrio

--- rio_sport.py
+++ rio_sport.py
@@ -1,3 +1,96 @@
+#
+# from selenium import webdriver
+# from fake_useragent import UserAgent
+# from selenium.webdriver.chrome.options import Options
+# import time
+# from selenium.webdriver.common.by import By
+# from tqdm import tqdm
+#
+#
+# for _ in tqdm(range(100)):
+#     # Создание экземпляра UserAgent
+#     ua = UserAgent()
+#     user_agent = ua.random
+#     # Опции браузера
+#     options = Options()
+#     # options.add_argument("--headless")  # Включение headless режима
+#     options.add_argument(f'user-agent={user_agent}')
+#     options.add_argument("--disable-blink-features=AutomationControlled")
+#     options.add_experimental_option("useAutomationExtension", False)
+#     options.add_experimental_option("excludeSwitches",["enable-automation"])
+#     # options.add_argument("--headless")
+#     # Путь к драйверу браузера (в данном случае Chrome)
+#     driver_path = 'chromeexe/chromedriver.exe'
+#     # Создание экземпляра драйвера
+#     driver = webdriver.Chrome()
+#     # url = 'https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html'
+#     # driver.get(url)
+#     # time.sleep(10)
+#
+#
+#     # Открытие ссылки
+#     url = 'https://rsport.ria.ru/20231219/sportswoman-of-year-1909228789.html'
+#     driver.get(url)
+#     time.sleep(2)
+#     element = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[4]/div[2]/div[7]/div[1]/div[1]')
+#     time.sleep(2)
+#     element.click()
+#     time.sleep(2)
+#     element = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[5]/div/div/div[2]/div/div[2]/div/div[8]/div/div/div[2]/div[1]/div[2]/div')
+#     time.sleep(2)
+#     element.click()
+#     time.sleep(2)
+#     element = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[6]/div/div/div[1]/div')
+#     time.sleep(2)
+#     element.click()
+#     time.sleep(2)
+#
+#     # Закрытие драйвера
+#     driver.quit()
+
+
+
+#
+# from selenium import webdriver
+# from fake_useragent import UserAgent
+# import time
+# from selenium.webdriver.common.by import By
+# import undetected_chromedriver as UC
+#
+# # Создание экземпляра UserAgent
+# ua = UserAgent()
+# user_agent = ua.random
+#
+# # Опции браузера
+# options = webdriver.ChromeOptions()
+# options.add_argument(f'user-agent={user_agent}')
+# options.add_argument("--disable-blink-features=AutomationControlled")
+# options.add_argument('start-maximized')
+# options.add_argument('disable-infobars')
+# # Другие опции браузера...
+#
+# # Путь к драйверу браузера (в данном случае Chrome)
+# driver_path = 'chromeexe/chromedriver.exe'
+#
+# # Создание экземпляра драйвера
+# # driver = webdriver.Chrome(options=options)
+# driver = UC.Chrome()
+# # Открытие ссылки
+# # url = 'https://www.whatismybrowser.com/detect/what-is-my-user-agent'
+# url='https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html'
+# driver.get(url)
+# time.sleep(100)
+#
+#
+#
+# # # Вывод User-Agent
+# # user_agent_element = driver.find_element(By.CSS_SELECTOR, '.string-major')
+# # print('User-Agent:', user_agent_element.text)
+#
+# # Закрытие драйвера
+# driver.quit()
+
+
 
 from selenium import webdriver
 from fake_useragent import UserAgent
@@ -11,43 +104,39 @@ for _ in tqdm(range(100)):
     # Создание экземпляра UserAgent
     ua = UserAgent()
     user_agent = ua.random
-
     # Опции браузера
     options = Options()
     # options.add_argument("--headless")  # Включение headless режима
-
     options.add_argument(f'user-agent={user_agent}')
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("useAutomationExtension", False)
+    options.add_experimental_option("excludeSwitches",["enable-automation"])
     # options.add_argument("--headless")
-
     # Путь к драйверу браузера (в данном случае Chrome)
     driver_path = 'chromeexe/chromedriver.exe'
-
-
     # Создание экземпляра драйвера
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options)
+    # url = 'https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html'
+    # driver.get(url)
+    # time.sleep(10)
 
 
     # Открытие ссылки
     url = 'https://rsport.ria.ru/20231219/sportswoman-of-year-1909228789.html'
-
     driver.get(url)
-    time.sleep(2)
-    # element = driver.find_element(By.CLASS_NAME,"text  text_ texo1703613919043 c1703613920162 ts_c1703613920046 c1703613920161")
-    element = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[4]/div[2]/div[7]/div[1]/div[1]')
-    time.sleep(2)
+    time.sleep(5)
+    element = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[4]/div[2]/div[8]/div[1]/div[1]/img')
+    time.sleep(5)
     element.click()
-    time.sleep(2)
-    element = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[5]/div/div/div[2]/div/div[2]/div/div[8]/div/div/div[2]/div[1]/div[2]/div')
-    time.sleep(2)
+    time.sleep(5)
+    element = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[5]/div/div/div[2]/div/div[2]/div/div[9]/div/div/div[2]/div[1]/div[2]/div')
+    time.sleep(5)
     element.click()
-    time.sleep(2)
+    time.sleep(5)
     element = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[6]/div/div/div[1]/div')
-    time.sleep(2)
+    time.sleep(5)
     element.click()
-    time.sleep(2)
+    time.sleep(5)
 
     # Закрытие драйвера
     driver.quit()
-
-
-
--- venv1/analize.py
+++ venv1/analize.py
@@ -1,81 +1,114 @@
-# from pulp import LpMaximize, LpProblem, LpStatus, LpVariable
+# # from pulp import LpMaximize, LpProblem, LpStatus, LpVariable
+# #
+# # # Создаем модель
+# # model = LpProblem(name="small-problem", sense=LpMaximize)
+# #
+# # # Инициализируем переменные
+# # x = LpVariable(name="x", lowBound=0) # Количество Продукта 1
+# # y = LpVariable(name="y", lowBound=2) # Количество Продукта 2
+# #
+# # # Добавляем ограничения
+# # model += (2 * x + 3 * y <= 20), "Машина 1"
+# #
+# # model += (4 * x + 1 * y <= 10, "Машина 2")
+# #
+# # # Добавляем целевую функцию
+# # model += 3 * x + 5 * y
+# #
+# #
+# # # Решаем задачу оптимизации
+# # status = model.solve()
+# # print('_____')
+# #
+# #
+# # # Выводим результаты
+# # print(f"status: {model.status}, {LpStatus[model.status]}")
+# # print(f"Прибыль: {model.objective.value()}")
+# # print(f"Продукт 1: {x.value()}")
+# # print(f"Продукт 2: {y.value()}")
+# # print('_____')
+# # print(model)
 #
-# # Создаем модель
-# model = LpProblem(name="small-problem", sense=LpMaximize)
+# # from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable
+# #
+# # # Создаем модель
+# # model = LpProblem(name="mixed-integer-problem", sense=LpMaximize)
+# #
+# # # Инициализируем переменные
+# # x = LpVariable(name="x", lowBound=0, cat="Integer")  # x - целочисленная переменная
+# # y = LpVariable(name="y", lowBound=0)  # y - непрерывная переменная
+# #
+# # # Добавляем ограничения в модель
+# # model += (x + 2 * y <= 20, "constraint_1")
+# # model += (3 * x - y >= 0, "constraint_2")
+# #
+# # # Добавляем целевую функцию
+# # model += lpSum([2*x, y])
+# #
+# # # Решаем задачу
+# # status = model.solve()
+# #
+# # # Выводим результат
+# # print(f"status: {model.status}, {LpStatus[model.status]}")
+# # print(f"objective: {model.objective.value()}")
+# # for var in model.variables():
+# #     print(f"{var.name}: {var.value()}")
+# # print('________')
+# # print(model)
+# #
+# # from sklearn import tree
+# # import pandas as pd
+# #
+# # # Данные: возраст машины и решение (1 - купить новую, 0 - оставить старую)
+# # data = pd.DataFrame({
+# #   'Age': [1, 2, 3, 4, 5, 6],
+# #   'Decision': [0, 0, 0, 1, 1, 1]
+# # })
+# #
+# # # Обучение дерева решений
+# # clf = tree.DecisionTreeClassifier()
+# # clf = clf.fit(data[['Age']], data['Decision'])
+# #
+# # # Предсказание решения для машины возрастом 3.5 года
+# # print(clf.predict([[101]]))
 #
-# # Инициализируем переменные
-# x = LpVariable(name="x", lowBound=0) # Количество Продукта 1
-# y = LpVariable(name="y", lowBound=2) # Количество Продукта 2
 #
-# # Добавляем ограничения
-# model += (2 * x + 3 * y <= 20), "Машина 1"
+# # from sklearn import tree
+# # from pyscipopt import Model
+# # import pandas as pd
+# #
+# # # Данные: возраст машины и решение (1 - купить новую, 0 - оставить старую)
+# # data = pd.DataFrame({
+# #   'Age': [1, 2, 3, 4, 5, 6],
+# #   'Decision': [0, 0, 0, 1, 1, 1]
+# # })
+# #
+# # # Обучение дерева решений
+# # clf = tree.DecisionTreeClassifier()
+# # clf = clf.fit(data[['Age']], data['Decision'])
+# #
+# # # Создаем модель
+# # model = Model("Car Decision")
+# #
+# # # Определяем переменную - возраст машины
+# # age = model.addVar("Age", vtype="I", lb=1, ub=20)  # ограничение: 1 <= age <= 20
+# #
+# # # Определяем целевую функцию - стоимость выбора машины, предсказанную деревом решений
+# # cost = clf.predict([[age]])[0]  # 0 - оставить старую, 1 - купить новую
+# #
+# # model.setObjective(cost, "minimize")
+# #
+# # # Решаем модель
+# # model.optimize()
+# #
+# # # Выводим оптимальное решение
+# # print("Optimal age:", model.getVal(age))
+# # print("Decision cost:", model.getObjVal())
 #
-# model += (4 * x + 1 * y <= 10, "Машина 2")
-#
-# # Добавляем целевую функцию
-# model += 3 * x + 5 * y
-#
-#
-# # Решаем задачу оптимизации
-# status = model.solve()
-# print('_____')
-#
-#
-# # Выводим результаты
-# print(f"status: {model.status}, {LpStatus[model.status]}")
-# print(f"Прибыль: {model.objective.value()}")
-# print(f"Продукт 1: {x.value()}")
-# print(f"Продукт 2: {y.value()}")
-# print('_____')
-# print(model)
-
-# from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable
-#
-# # Создаем модель
-# model = LpProblem(name="mixed-integer-problem", sense=LpMaximize)
-#
-# # Инициализируем переменные
-# x = LpVariable(name="x", lowBound=0, cat="Integer")  # x - целочисленная переменная
-# y = LpVariable(name="y", lowBound=0)  # y - непрерывная переменная
-#
-# # Добавляем ограничения в модель
-# model += (x + 2 * y <= 20, "constraint_1")
-# model += (3 * x - y >= 0, "constraint_2")
-#
-# # Добавляем целевую функцию
-# model += lpSum([2*x, y])
-#
-# # Решаем задачу
-# status = model.solve()
-#
-# # Выводим результат
-# print(f"status: {model.status}, {LpStatus[model.status]}")
-# print(f"objective: {model.objective.value()}")
-# for var in model.variables():
-#     print(f"{var.name}: {var.value()}")
-# print('________')
-# print(model)
-#
-# from sklearn import tree
-# import pandas as pd
-#
-# # Данные: возраст машины и решение (1 - купить новую, 0 - оставить старую)
-# data = pd.DataFrame({
-#   'Age': [1, 2, 3, 4, 5, 6],
-#   'Decision': [0, 0, 0, 1, 1, 1]
-# })
-#
-# # Обучение дерева решений
-# clf = tree.DecisionTreeClassifier()
-# clf = clf.fit(data[['Age']], data['Decision'])
-#
-# # Предсказание решения для машины возрастом 3.5 года
-# print(clf.predict([[101]]))
-
-
 # from sklearn import tree
 # from pyscipopt import Model
 # import pandas as pd
+# import numpy as np
 #
 # # Данные: возраст машины и решение (1 - купить новую, 0 - оставить старую)
 # data = pd.DataFrame({
@@ -92,55 +125,58 @@
 #
 # # Определяем переменную - возраст машины
 # age = model.addVar("Age", vtype="I", lb=1, ub=20)  # ограничение: 1 <= age <= 20
+# print(type(age))
+# # Получаем "правила" от дерева решений
+# ages = np.arange(1, 21)  # возраста от 1 до 20
+# decisions = clf.predict(ages.reshape(-1, 1))  # предсказания для каждого возраста
 #
-# # Определяем целевую функцию - стоимость выбора машины, предсказанную деревом решений
-# cost = clf.predict([[age]])[0]  # 0 - оставить старую, 1 - купить новую
+# # Добавляем "правила" в модель в виде ограничений
+# for a, d in zip(ages, decisions):
+#     if d == 0:  # если нужно купить новую машину
 #
-# model.setObjective(cost, "minimize")
+#         model.addCons(age >= a)  # возраст машины должен быть не меньше a
+#
+# # Определяем целевую функцию (в данном случае просто минимизируем возраст)
+# model.setObjective(age, "minimize")
 #
 # # Решаем модель
 # model.optimize()
 #
 # # Выводим оптимальное решение
 # print("Optimal age:", model.getVal(age))
-# print("Decision cost:", model.getObjVal())
 
-from sklearn import tree
-from pyscipopt import Model
-import pandas as pd
+
 import numpy as np
+import pandas as pd
+import statsmodels.api as sm
 
-# Данные: возраст машины и решение (1 - купить новую, 0 - оставить старую)
-data = pd.DataFrame({
-  'Age': [1, 2, 3, 4, 5, 6],
-  'Decision': [0, 0, 0, 1, 1, 1]
-})
+# Генерируем случайные данные
+np.random.seed(0)
+N = 100 # количество наблюдений
+K = 3 # количество признаков
+X = np.random.rand(N, K)
+# Генерируем метки классов
+y = np.random.choice([0, 1, 2], N)
 
-# Обучение дерева решений
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(data[['Age']], data['Decision'])
+# Преобразуем в pandas DataFrame
+df = pd.DataFrame(X, columns=[f'feature{i+1}' for i in range(K)])
+df['target'] = y
 
-# Создаем модель
-model = Model("Car Decision")
+# Создаем матрицу признаков и целевую переменную
+X = df[['feature1', 'feature2', 'feature3']]
+y = df['target']
+print(df)
 
-# Определяем переменную - возраст машины
-age = model.addVar("Age", vtype="I", lb=1, ub=20)  # ограничение: 1 <= age <= 20
-print(type(age))
-# Получаем "правила" от дерева решений
-ages = np.arange(1, 21)  # возраста от 1 до 20
-decisions = clf.predict(ages.reshape(-1, 1))  # предсказания для каждого возраста
+# Добавляем константу для перехвата
+X = sm.add_constant(X)
+print(X)
 
-# Добавляем "правила" в модель в виде ограничений
-for a, d in zip(ages, decisions):
-    if d == 0:  # если нужно купить новую машину
+# Создаем MNL модель
+mnl_model = sm.MNLogit(y, X)
 
-        model.addCons(age >= a)  # возраст машины должен быть не меньше a
+print(mnl_model)
+# Обучаем модель
+result = mnl_model.fit()
 
-# Определяем целевую функцию (в данном случае просто минимизируем возраст)
-model.setObjective(age, "minimize")
-
-# Решаем модель
-model.optimize()
-
-# Выводим оптимальное решение
-print("Optimal age:", model.getVal(age))
+# Выводим результаты
+print(result.summary())
