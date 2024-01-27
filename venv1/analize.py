2c2852230c19313ec8650beb80b7dc8562539355
Python <svetloewill@mail.ru>
2024-01-27 18:57:37 +0300

    3

--- venv1/analize.py
+++ venv1/analize.py
@@ -373,3 +373,17 @@ import statsmodels.api as sm
 #
 # # Use hvplot to create a line plot
 # df.hvplot.line('x', 'y')
+
+# from sklearn.model_selection import TimeSeriesSplit
+# import numpy as np
+#
+# # Создадим пример данных временных рядов
+# X = np.array([[1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4]])
+# y = np.array([1, 2, 3, 4, 5, 6])
+#
+# tscv = TimeSeriesSplit(n_splits=5)
+#
+# for train_index, test_index in tscv.split(X):
+#     print("TRAIN:", train_index, "TEST:", test_index)
+#     X_train, X_test = X[train_index], X[test_index]
+#     y_train, y_test = y[train_index], y[test_index]
